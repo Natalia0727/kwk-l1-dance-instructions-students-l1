d7717c9f931745325ba55ceaddad9def8f03a9ca
Natalia0727 <oicafn@gmail.com>
2018-06-04 23:23:36 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -18,4 +18,5 @@ def Gangdam_Style
   puts "Cross your right wrist over your left and hold them together"
   puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
   puts "Lasso"
-  puts ""
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  
