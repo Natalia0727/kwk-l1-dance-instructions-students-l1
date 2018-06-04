76ca6f4a7321c72e8bb27207aa57d5b277117579
Natalia0727 <oicafn@gmail.com>
2018-06-04 23:18:31 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -3,4 +3,6 @@ def Gangdam_Style
   puts "1 Plant legs far apart, bend knees slightly and keep posture loose"
   puts "Lift right foot"
   puts "Return right foot to the ground"
-  puts ""
+  puts "Finishing with a small skip-step backward"
+  puts "skip-step"
+  
