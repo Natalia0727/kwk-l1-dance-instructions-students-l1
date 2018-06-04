b86616a1d4bc2e0571134e4a43c89c02acb99785
Natalia0727 <oicafn@gmail.com>
2018-06-04 23:27:49 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -23,4 +23,7 @@ def Gangdam_Style
   puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
   puts "Be cool!"
   puts "overall".uppercase
+  puts "Assume Stance"
+  puts "Bust a move"
+  puts "Footwork"
   
